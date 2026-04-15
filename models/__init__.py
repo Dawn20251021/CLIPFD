bc9e4cccf34b0f9685c1b0c7bc0567094ce9d9b0
Dawn20251021 <2396454426@qq.com>
2026-04-15 09:14:19 +0800

    新增

---
+++ models/__init__.py
@@ -0,0 +1,2 @@
+def get_model(name):
+        raise NotImplementedError("build_model is not implemented yet.")
---
+++ models/reconstruct_CLIP.py
@@ -0,0 +1,36 @@
+from .orign_CLIP_model import clip
+import torch.nn as nn
+
+
+
+# 由于是直接使用的clip源码，但是只用到了图像编码器部分，因此重构一下该部分即可；
+class CLIPModel(nn.Module):
+    def __init__(self, name="ViT-L/14", freeze=False,device="cpu"):
+        super().__init__()
+
+        self.backbone, self.preprocess = clip.load(name, device=device)
+        self.backbone_name = name
+        self.global_dim = 768
+        self.local_dim = 1024
+
+        if freeze:
+            self.freeze()
+
+    def extract_global_feat(self, x):
+        return self.backbone.encode_image(x)
+
+    def freeze(self):
+        for p in self.backbone.parameters():
+            p.requires_grad = False
+
+    def unfreeze(self):
+        for p in self.backbone.parameters():
+            p.requires_grad = True
+
+    def forward(self, x):
+        global_feat = self.extract_global_feat(x)
+        return {
+            "global_feat": global_feat
+        }
+
+
